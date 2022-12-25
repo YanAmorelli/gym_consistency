b8d6d96664539a1762489693bb745ec2d6189ca1
Rafael de Melo <rafael5665@hotmail.com>
2022-12-25 19:59:57 -0300

    Function to validate user data

--- models/model_user/user.go
+++ models/model_user/user.go
@@ -1,8 +1,9 @@
 package model_user
 
 type User struct {
-	FullName string `json:"fullname" gorm:"column:fullname"`
-	Username string `json:"username" gorm:"column:username"`
-	Email    string `json:"email" gorm:"column:email"`
-	Password string `json:"passwd" gorm:"column:passwd"`
+	FullName        string `json:"fullname" gorm:"column:fullname"`
+	Username        string `json:"username" gorm:"column:username"`
+	Email           string `json:"email" gorm:"column:email"`
+	Password        string `json:"passwd" gorm:"column:passwd"`
+	ConfirmPassword string `json:"confirmation_password" gorm:"-"`
 }
---
+++ services/validation/validation.go
@@ -0,0 +1,35 @@
+package validation
+
+import (
+	"errors"
+
+	"github.com/yanamorelli/gym_consistency/models/model_user"
+)
+
+func ValidateUserData(user *model_user.User) error {
+	if user.Username == "" {
+		return errors.New("username not provided")
+	}
+
+	if user.Email == "" {
+		return errors.New("email not provided")
+	}
+
+	if user.FullName == "" {
+		return errors.New("fullname not provided")
+	}
+
+	if user.Password == "" {
+		return errors.New("password not provided")
+	}
+
+	if user.ConfirmPassword == "" {
+		return errors.New("confirmation password not provided")
+	}
+
+	if user.ConfirmPassword != user.Password {
+		return errors.New("passwords doesn't match")
+	}
+
+	return nil
+}
