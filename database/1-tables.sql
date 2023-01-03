6a8171ed875fe00d1dd852dff7a060eda2e04a76
caioabreu11 <caio.abreu@trescon.com.br>
2023-01-03 11:47:15 -0300

    renames on presence to attendance.

--- database/1-tables.sql
+++ database/1-tables.sql
@@ -1,17 +1,17 @@
 CREATE TABLE user_info (
-    user_id   bigint NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    fullname     character varying(100),
-    username     character varying(30),
-    passwd       text,
-    email        character varying(100)
+    user_id        BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    fullname       VARCHAR(100),
+    username       VARCHAR(30),
+    passwd         TEXT,
+    email          VARCHAR(100)
 );
 
 CREATE TABLE user_attendance (
-	 presence_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	 dt_presence DATE ,
-	 went_gym    BOOL,
-	 user_id INT references user_info(user_id)
+	 attendance_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	 dt_attendance DATE ,
+	 went_gym      BOOL,
+	 user_id       INT references user_info(user_id)
 );
 
-CREATE UNIQUE INDEX dt_presence_user_id ON user_presence 
-(dt_presence, user_id);
+CREATE UNIQUE INDEX dt_attendance_user_id ON user_attendance 
+(dt_attendance, user_id);
