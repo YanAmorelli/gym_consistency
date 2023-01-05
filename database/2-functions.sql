ea7b8b011afac3be07c5f42c6c2dba4b20631fe4
caioabreu11 <caio.abreu@trescon.com.br>
2023-01-05 12:56:36 -0300

    changing function to login user.

--- database/2-functions.sql
+++ database/2-functions.sql
@@ -13,18 +13,24 @@ ON public.user_info
 FOR EACH ROW
 EXECUTE FUNCTION public.crpt_passwd();
 
-CREATE OR REPLACE FUNCTION login_user(
-	passwd_inform text,
-	passwd_saved text)
-    RETURNS boolean
-    LANGUAGE 'plpgsql'
-    COST 100
-    VOLATILE PARALLEL UNSAFE
-AS $BODY$
+CREATE OR REPLACE FUNCTION auth_login(p_user text,p_passwd text) 
+RETURNS boolean
+LANGUAGE 'plpgsql'
+AS $$
 DECLARE 
-	auth	boolean;
-BEGIN 
-auth:= passwd_saved = crypt(passwd_inform, passwd_saved );
-RETURN auth;
+    auth	boolean;
+BEGIN
+SELECT 
+	(passwd = crypt(p_passwd,passwd)) AS authed_login
+INTO 
+	auth
+FROM 
+	user_info
+WHERE
+	username = p_user;
+IF auth IS NOT true THEN  
+	RETURN false;
+		ELSE RETURN true;
+END IF;
 END;
-$BODY$;
+$$;
