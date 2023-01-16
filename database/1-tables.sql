650698731922e06a9d88291ee59c2624047e9555
caioabreu11 <caio.abreu@trescon.com.br>
2023-01-16 19:10:58 -0300

    changes made on request table.

--- database/1-tables.sql
+++ database/1-tables.sql
@@ -1,27 +1,29 @@
 CREATE TABLE user_info (
-    user_id         BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    fullname        VARCHAR(100),
-    username        VARCHAR(30),
-    passwd          TEXT,
-    email           VARCHAR(100)
+    user_id             BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    fullname            VARCHAR(100),
+    username            VARCHAR(30),
+    passwd              TEXT,
+    email               VARCHAR(100)
 );
 
 CREATE TABLE user_attendance (
-	 attendance_id  BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	 dt_attendance  DATE ,
-	 went_gym       BOOL,
-	 user_id        INT references user_info(user_id)
+	 attendance_id    BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	 dt_attendance    DATE ,
+	 went_gym         BOOL,
+	 user_id          INT references user_info(user_id)
 );
 
 CREATE TABLE friend_request (
-      request_id    INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-      user_sent     INT,
-      user_received INT
+      request_id        INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+      user_sent         INT,
+      user_received     INT,
+      request_status    BOOL,
+      dt_sented         timestamptz DEFAULT NOW();
 );
 
 CREATE TABLE user_friendship (
-      person        INT,
-      friend        INT,
+      person            INT,
+      friend            INT,
       PRIMARY KEY (person,friend)
 );
 
