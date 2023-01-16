84923642b3d0af8c28457c2729ab31df50af75fb
caioabreu11 <caio.abreu@trescon.com.br>
2023-01-16 17:44:35 -0300

    Tables friendship themed.

--- database/1-tables.sql
+++ database/1-tables.sql
@@ -1,16 +1,28 @@
 CREATE TABLE user_info (
-    user_id        BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    fullname       VARCHAR(100),
-    username       VARCHAR(30),
-    passwd         TEXT,
-    email          VARCHAR(100)
+    user_id         BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    fullname        VARCHAR(100),
+    username        VARCHAR(30),
+    passwd          TEXT,
+    email           VARCHAR(100)
 );
 
 CREATE TABLE user_attendance (
-	 attendance_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	 dt_attendance DATE ,
-	 went_gym      BOOL,
-	 user_id       INT references user_info(user_id)
+	 attendance_id  BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	 dt_attendance  DATE ,
+	 went_gym       BOOL,
+	 user_id        INT references user_info(user_id)
+);
+
+CREATE TABLE friend_request (
+      request_id    INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+      user_sent     INT,
+      user_received INT
+);
+
+CREATE TABLE user_friendship (
+      person        INT,
+      friend        INT,
+      PRIMARY KEY (person,friend)
 );
 
 CREATE UNIQUE INDEX dt_attendance_user_id ON user_attendance 
